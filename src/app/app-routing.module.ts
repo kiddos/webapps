da6eaed3e21724d4d0a8f7a0c8079ee998cb9477
kiddos <kiddo831007@gmail.com>
2019-10-16 02:07:35 +0800

    add chase component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChaseComponent } from './chase/chase.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChaseComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/chase/chase.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+
+
+@Component({
+  selector: 'app-chase',
+  templateUrl: './chase.component.html',
+  styleUrls: ['./chase.component.css']
+})
+export class ChaseComponent implements OnInit {
+  @ViewChild('it', { static: false }) it: ElementRef<HTMLCanvasElement>;
+
+  private init: boolean;
+  private moving: boolean;
+  private x: number;
+  private y: number;
+  private targetX: number;
+  private targetY: number;
+
+  constructor(private renderer: Renderer2) {
+    this.init = false;
+    this.x = 0;
+    this.y = 0;
+    this.moving = false;
+  }
+
+  ngOnInit() { }
+
+
+  move(event: MouseEvent) {
+    if (!this.init) {
+      this.x = this.it.nativeElement.offsetLeft;
+      this.y = this.it.nativeElement.offsetTop;
+
+      this.init = true;
+    }
+
+    this.targetX = event.clientX;
+    this.targetY = event.clientY;
+    if (!this.moving) {
+      this.moving = true;
+      this.moveToTarget();
+    }
+  }
+
+  clip(value: number, min: number, max: number) : number {
+    if (value > 0) {
+      return Math.max(Math.min(max, value), min);
+    } else {
+      return Math.min(Math.max(-max, value), -min);
+    }
+  }
+
+  moveToTarget() {
+    let dx = this.targetX - this.x;
+    let dy = this.targetY - this.y;
+    let angle = Math.atan2(dy, dx);
+    let dist = Math.sqrt(dx * dx + dy * dy);
+
+    this.renderer.setStyle(this.it.nativeElement, 'transform', `rotate(${angle}rad)`);
+
+    if (dist > this.it.nativeElement.width / 2.0) {
+      const delta = this.clip(dist * 0.1, 0, 3);
+      dx = delta * Math.cos(angle);
+      dy = delta * Math.sin(angle);
+
+      this.x += dx;
+      this.y += dy;
+      this.it.nativeElement.style.left = this.x + 'px';
+      this.it.nativeElement.style.top = this.y + 'px';
+
+      setTimeout(() => {
+        this.moveToTarget();
+      }, 10);
+    } else {
+      this.moving = false;
+    }
+  }
+}
