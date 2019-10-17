2e4112566d21d27d48afba5335e8f3d2c3b5fb90
kiddos <kiddo831007@gmail.com>
2019-10-17 16:11:02 +0800

    add tic tac toe route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { ChaseComponent } from './chase/chase.component';
 import { ConversionComponent } from './conversion/conversion.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 
 
 const routes: Routes = [
   { path: '', component: ChaseComponent },
   { path: 'conversion', component: ConversionComponent },
+  { path: 'tic-tac-toe', component: TicTacToeComponent },
 ];
 
 @NgModule({
