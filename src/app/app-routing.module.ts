08ac67da7f5f9fd1647667ef0141914ee58892a6
kiddos <kiddo831007@gmail.com>
2019-10-17 15:28:31 +0800

    add conversion route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChaseComponent } from './chase/chase.component';
+import { ConversionComponent } from './conversion/conversion.component';
 
 
 const routes: Routes = [
   { path: '', component: ChaseComponent },
+  { path: 'conversion', component: ConversionComponent },
 ];
 
 @NgModule({
