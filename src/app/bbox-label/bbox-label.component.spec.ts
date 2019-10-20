537e5ee2f4eceee7a43a9bb3eb72288345a1e758
kiddos <kiddo831007@gmail.com>
2019-10-21 05:43:24 +0800

    add required module for testing

--- src/app/bbox-label/bbox-label.component.spec.ts
+++ src/app/bbox-label/bbox-label.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BBoxLabelComponent } from './bbox-label.component';
+import { BBoxLabelComponent, HelpDialog } from './bbox-label.component';
+
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('BboxLabelComponent', () => {
   let component: BBoxLabelComponent;
@@ -8,7 +16,19 @@ describe('BboxLabelComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BBoxLabelComponent ]
+      imports: [
+        FormsModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatRadioModule,
+        MatInputModule,
+        MatIconModule,
+      ],
+      declarations: [
+        BBoxLabelComponent,
+        HelpDialog,
+      ]
     })
     .compileComponents();
   }));
