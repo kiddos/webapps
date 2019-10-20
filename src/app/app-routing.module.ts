89c571701c0b5a66c117636261703d35c4c01bfb
kiddos <kiddo831007@gmail.com>
2019-10-21 05:15:13 +0800

    add title data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,30 @@ import { Routes, RouterModule } from '@angular/router';
 import { ChaseComponent } from './chase/chase.component';
 import { ConversionComponent } from './conversion/conversion.component';
 import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
+import { BBoxLabelComponent } from './bbox-label/bbox-label.component';
 
 
 const routes: Routes = [
-  { path: '', component: ChaseComponent },
-  { path: 'conversion', component: ConversionComponent },
-  { path: 'tic-tac-toe', component: TicTacToeComponent },
+  {
+    path: '',
+    component: ChaseComponent,
+    data: { title: "kiddo's webapps"},
+  },
+  {
+    path: 'conversion',
+    component: ConversionComponent,
+    data: { title: 'Conversion' },
+  },
+  {
+    path: 'tic-tac-toe',
+    component: TicTacToeComponent,
+    data: { title: 'Tic Tac Toe'},
+  },
+  {
+    path: 'bbox-label',
+    component: BBoxLabelComponent,
+    data: { title: 'Bounding Box Label'},
+  },
 ];
 
 @NgModule({
